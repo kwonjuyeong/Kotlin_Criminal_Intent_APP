ff9e5a128137f1fb43b2af767f708643b2f86279
kwonjuyeong <20161527@vision.hoseo.edu>
2023-02-02 23:51:51 +0900

    ㅜ

--- app/src/main/java/com/example/study_kotlin2/CriminalIntent.kt
+++ app/src/main/java/com/example/study_kotlin2/CriminalIntent.kt
@@ -3,13 +3,11 @@ package com.example.study_kotlin2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CriminalIntent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        //commit test
 
 
     }
